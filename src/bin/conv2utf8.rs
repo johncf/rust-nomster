6d4d80a6847231311fb6e2b76f4ed0d7d632888e
johncf <johncf@protonmail.com>
2018-03-01 20:43:20 +0530

    minor update

--- src/bin/conv2utf8.rs
+++ src/bin/conv2utf8.rs
@@ -3,7 +3,7 @@ extern crate parster;
 #[macro_use]
 extern crate structopt;
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -11,11 +11,12 @@ use structopt::StructOpt;
 struct Opt {
     #[structopt(name = "INFILE", help = "webster raw file (dos format)", parse(from_os_str))]
     input: PathBuf,
-    #[structopt(name = "OUTFILE", help = "output file path", parse(from_os_str))]
-    output: PathBuf,
+    #[structopt(name = "OUTFILE", help = "patched output (overwrites input if not given)",
+                parse(from_os_str))]
+    output: Option<PathBuf>,
 }
 
-pub fn write_unix<P: AsRef<std::path::Path>>(contents: &str, output: P) -> Result<(), std::io::Error> {
+fn write_unix(contents: &str, output: &Path) -> Result<(), std::io::Error> {
     use std::io::Write;
     let contents = contents.replace("\r\n", "\n");
     std::fs::File::create(output)?.write_all(contents.as_bytes())?;
@@ -24,6 +25,7 @@ pub fn write_unix<P: AsRef<std::path::Path>>(contents: &str, output: P) -> Resul
 
 fn main() {
     let opt = Opt::from_args();
-    let contents = parster::read_dos(opt.input).unwrap();
-    write_unix(&contents, opt.output).unwrap();
+    let output = opt.output.as_ref().unwrap_or(&opt.input);
+    let contents = parster::read_dos(&opt.input).unwrap();
+    write_unix(&contents, output).unwrap();
 }
