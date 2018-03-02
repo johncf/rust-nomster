de58ef085b443a47b5ecc8e8cd4aef317a006ebf
johncf <johncf@protonmail.com>
2018-03-02 20:25:21 +0530

    restart!

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#[cfg(feature="dosformat")]
-extern crate encoding;
 #[macro_use]
 extern crate nom;
 
@@ -10,19 +8,6 @@ use std::path::Path;
 mod parser;
 
 pub use parser::parse;
-
-#[cfg(feature="dosformat")]
-pub fn read_dos<P: AsRef<Path>>(path: P) -> Result<String, Error> {
-    let mut contents = Vec::with_capacity(2 << 20);
-    File::open(path)?.read_to_end(&mut contents)?;
-    let (dec_res, enc_ref) = encoding::decode(&contents,
-                                              encoding::DecoderTrap::Strict,
-                                              encoding::all::WINDOWS_1252);
-    dec_res.map_err(|e| {
-        std::io::Error::new(std::io::ErrorKind::InvalidData,
-                            format!("{} decoding error: {}", enc_ref.name(), e))
-    })
-}
 
 pub fn read_unix<P: AsRef<Path>>(path: P) -> Result<String, Error> {
     let mut contents = String::with_capacity(2 << 20);
